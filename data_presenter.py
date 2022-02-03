7f4f3c79454708c1615c84dc8d38187253a634f5
Anthony <adominguez15722@gmail.com>
2022-02-03 15:26:39 -0600

    Part 2 complete

--- data_presenter.py
+++ data_presenter.py
@@ -1,7 +1,29 @@
+from fileinput import close
+from functools import total_ordering
+
+
 open_file = open("CupcakeInvoices.csv")
 
+full_total = []
 for row in open_file:
-    print(row)
+    # print(row)
+    
+    # row = row.split(',')
+    # print(row[2])
+    
+    # row = row.split(',')
+    # amount = int(row[3])
+    # price = float(row[4])
+    # print(amount * price)
+
+    row = row.split(',')
+    amount = int(row[3])
+    price = round(float(row[4]), 2)
+    total = round((amount * price), 2)
+    full_total.append(total)
 
 
+print (round(sum(full_total)))
+
+open_file.close()
 
